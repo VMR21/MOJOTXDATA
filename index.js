7038e42967c90a7493afe97e464ac3538f22fb73
VMR21 <reddymodith@gmail.com>
2025-07-22 09:01:45 +0900

    Update index.js

--- index.js
+++ index.js
@@ -62,8 +62,8 @@ setInterval(fetchAndCacheData, 5 * 60 * 1000);
 app.get("/leaderboard/top14", (req, res) => {
   const injectedUser = {
     username: maskUsername("VirgzilZos"),
-    wagered: 129881,
-    weightedWager: 129881
+    wagered: 176866,
+    weightedWager: 176866
   };
 
   // Remove existing masked VirgzilZos
